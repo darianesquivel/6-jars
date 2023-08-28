bba2375397b0880a84ba179164da9e6bd26c45db
Darian <darianesquivelf@gmail.com>
2023-08-28 01:01:45 -0300

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { AuthContextProvider } from './context/AuthContext';
 import Routes from './routes/Routes'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+// const lightTheme = createTheme({
+//   palette: {
+//     mode: 'light',
+//   },
+// });
+
 function App() {
   return (
-    <AuthContextProvider>
-      <Routes />
-    </AuthContextProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AuthContextProvider>
+        <Routes />
+      </AuthContextProvider>
+    </ThemeProvider>
+
   );
 }
 
